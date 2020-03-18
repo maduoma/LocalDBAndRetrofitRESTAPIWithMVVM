882018ab8aeacc6ac3e1eae421ec91b83b89ff7b
Maduabughichi Achilefu <aachilefu@yahoo.com>
2020-03-18 03:47:30 +0100

    Local DB And Retrofit REST API With MVVM

--- app/src/main/java/com/dodemy/android/localdbandretrofitrestapiwithmvvm/adapters/CategoryViewHolder.java
+++ app/src/main/java/com/dodemy/android/localdbandretrofitrestapiwithmvvm/adapters/CategoryViewHolder.java
@@ -36,7 +36,7 @@ public class CategoryViewHolder extends RecyclerView.ViewHolder implements View.
 
     public void onBind(Recipe recipe){
 
-        Uri path = Uri.parse("android.resource://com.codingwithmitch.foodrecipes/drawable/" + recipe.getImage_url());
+        Uri path = Uri.parse("android.resource://com.dodemy.android.localdbandretrofitrestapiwithmvvm/drawable/" + recipe.getImage_url());
         requestManager
                 .load(path)
                 .into(categoryImage);
